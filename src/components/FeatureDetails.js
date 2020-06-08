54604228197e243414200de572cbef9ea22d3db1
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-06-08 16:41:23 +1200

    fixed visual bug when dealing with features with varying numbers of lines

--- src/components/FeatureDetails.js
+++ src/components/FeatureDetails.js
@@ -2,7 +2,12 @@ import React, { useContext } from 'react';
 
 const FeatureDetails = ({feature}) => {
     return (
-        <div className="feature">{feature.feature}</div>
+        <div className="feature">
+            <div className="featureContainer">
+                <div className="featureTitle">{feature.feature}</div>
+                <hr/>
+            </div>
+        </div>
     );
 }
  
