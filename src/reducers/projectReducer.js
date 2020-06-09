de20be86cdfd1b39f9ba5243c95614f0a4381f4e
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-06-09 16:45:21 +1200

    can now add features to projects

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -2,6 +2,21 @@ import { v1 as uuidv1 } from 'uuid';
 
 export const projectReducer = (state, action) => {
     switch(action.type) {
+        case 'ADD_FEATURE':
+            return state.map(project => {
+                if(project.id === action.addFeature.projectId) {
+                    return project = {
+                        title: project.title,
+                        features: [...project.features, {
+                            feature: action.addFeature.feature,
+                            id: uuidv1()
+                        }],
+                        id: project.id
+                    }
+                } else {
+                    return project
+                }
+            })
         default:
             return state
     }
