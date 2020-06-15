b685f8103492f7f6b9785d273dfcbd031adad91e
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-06-15 14:38:56 +1200

    Updates

---
+++ precache-manifest.ed5651b9235caa3d9ada87efe227d743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e0dd785c9b0a0912c42352ccc945fd",
+    "url": "/React-ProjectManager/index.html"
+  },
+  {
+    "revision": "05ab36a738bc197278ee",
+    "url": "/React-ProjectManager/static/css/main.be8d272e.chunk.css"
+  },
+  {
+    "revision": "a7129d99d49e256fd77b",
+    "url": "/React-ProjectManager/static/js/2.f01dd5b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ProjectManager/static/js/2.f01dd5b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ab36a738bc197278ee",
+    "url": "/React-ProjectManager/static/js/main.78451526.chunk.js"
+  },
+  {
+    "revision": "b6d4d8d33f8d13e8389b",
+    "url": "/React-ProjectManager/static/js/runtime-main.47e1bbd6.js"
+  }
+]);
