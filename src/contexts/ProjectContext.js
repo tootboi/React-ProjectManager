0ba04b9770ede50f7618095b839c3212f11b0f3b
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-07-16 14:05:33 +1200

    reformatted the data structure to use react-beautiful-dnd

--- src/contexts/ProjectContext.js
+++ src/contexts/ProjectContext.js
@@ -4,16 +4,41 @@ import { projectReducer } from '../reducers/projectReducer';
 export const ProjectContext = createContext();
 
 const ProjectContextProvider = (props) => {
+    const initialData = [
+        {title: 'test project',
+        //sub features
+        tasks: {
+            'unique1-1': {id: 'unique1-1', content: 'Test1'},
+            'unique1-2': {id: 'unique1-2', content: 'Test2'},
+            'unique1-3': {id: 'unique1-3', content: 'Test3'},
+            'unique1-4': {id: 'unique1-4', content: 'Test4'},
+        },
+        //features
+        features: {
+            'unique1': {
+                id: 'unique1',
+                title: 'test feature',
+                taskIds: ['unique1-1', 'unique1-2'],
+                tasksDone: 0,
+                doneTasks: [],
+            },
+            'unique2': {
+                id: 'unique2',
+                title: 'test feature2',
+                taskIds: ['unique1-3', 'unique1-4'],
+                tasksDone: 0,
+                doneTasks: [],
+            },
+        },
+        featureOrder: ['unique1', 'unique2'],
+        featuresDone: 0, 
+        doneFeatures: [],
+        id: 'unique0'},
+    ]
     const [projects, dispatch] = useReducer(projectReducer, [], () => {
         const localData = localStorage.getItem('projects');
-        return localData ? JSON.parse(localData) : [
-            {title: 'test project', features: [
-                {feature: 'feature1', id: 'unique1-1'},
-                {feature: 'feature2', id: 'unique1-2'},
-                {feature: 'feature3', id: 'unique1-3'},
-                {feature: 'feature4', id: 'unique1-4'},
-            ], totalDone: 0, isDone: [], id: 'unique1'},
-        ]
+        return localData ? JSON.parse(localData) :
+            initialData
     });
 
     useEffect(() => {
