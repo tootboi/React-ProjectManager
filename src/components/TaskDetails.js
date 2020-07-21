48d0edf683ff21af66761f94e0be3d63c721e768
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-07-21 14:56:09 +1200

    task content now editable

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,10 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import { ProjectContext } from '../contexts/ProjectContext';
+import TextareaAutosize from 'react-textarea-autosize';
 
 const TaskDetails = ({ task, index, feature, project }) => {
     const { dispatch } = useContext(ProjectContext);
+    const [newTask, setNewTask] = useState(task.content);
 
+    const editTask = (e) => {
+        setNewTask(e.target.value);
+        dispatch({type: 'EDIT_TASK', editTask: {projectId: project.id, taskId: task.id, taskContent: e.target.value}});
+    }
     const deleteTask = (e) => {
         dispatch({type: 'DELETE_TASK', ids: {projectId: project.id, featureId: feature.id, taskId: task.id}});
     }
@@ -12,7 +18,9 @@ const TaskDetails = ({ task, index, feature, project }) => {
         <Draggable draggableId={task.id} index={index}>
             {(provided) => (
                 <div className="tasks" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                    <span className='dot'></span>{task.content}<span className='deleteBtn' onClick={deleteTask}>&times;</span>
+                    <span className='dot'></span>
+                    <TextareaAutosize className='taskText' minRows={1} maxRows={5} value={newTask} onChange={editTask} />
+                    <span className='deleteBtn' onClick={deleteTask}>&times;</span>
                 </div>
             )}
         </Draggable>
