2b926b468468cf25fa41b400eaa528c1f7cd637b
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-07-23 14:29:52 +1200

    now using contentEditable div instead of a textarea for taskText

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -1,15 +1,15 @@
 import React, { useContext, useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import { ProjectContext } from '../contexts/ProjectContext';
-import TextareaAutosize from 'react-textarea-autosize';
+import ContentEditable from 'react-simple-contenteditable';
 
 const TaskDetails = ({ task, index, feature, project }) => {
     const { dispatch } = useContext(ProjectContext);
     const [newTask, setNewTask] = useState(task.content);
 
-    const editTask = (e) => {
-        setNewTask(e.target.value);
-        dispatch({type: 'EDIT_TASK', editTask: {projectId: project.id, taskId: task.id, taskContent: e.target.value}});
+    const editTask =(e, value) => {
+        setNewTask(value);
+        dispatch({type: 'EDIT_TASK', editTask: {projectId: project.id, taskId: task.id, taskContent: value}});
     }
     const deleteTask = (e) => {
         dispatch({type: 'DELETE_TASK', ids: {projectId: project.id, featureId: feature.id, taskId: task.id}});
@@ -19,7 +19,12 @@ const TaskDetails = ({ task, index, feature, project }) => {
             {(provided) => (
                 <div className="tasks" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                     <span className='dot'></span>
-                    <TextareaAutosize className='taskText' minRows={1} maxRows={5} value={newTask} onChange={editTask} />
+                    <ContentEditable
+                        html={newTask}
+                        className='taskText'
+                        onChange={editTask}
+                        contentEditable="plaintext-only"
+                    />
                     <span className='deleteBtn' onClick={deleteTask}>&times;</span>
                 </div>
             )}
