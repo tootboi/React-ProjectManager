f0c91fa232d942a18b585429e09469314c4b65f0
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-07-29 16:33:50 +1200

    app displays instructions when it is opened for the first time

--- src/contexts/ProjectContext.js
+++ src/contexts/ProjectContext.js
@@ -5,27 +5,29 @@ export const ProjectContext = createContext();
 
 const ProjectContextProvider = (props) => {
     const initialData = [
-        {title: 'test project',
+        {title: 'project title',
         //sub features
         tasks: {
-            'unique1-1': {id: 'unique1-1', content: 'Test1'},
-            'unique1-2': {id: 'unique1-2', content: 'Test2'},
-            'unique1-3': {id: 'unique1-3', content: 'Test3'},
-            'unique1-4': {id: 'unique1-4', content: 'Test4'},
+            'unique1-0': {id: 'unique1-0', content: 'Task'},
+            'unique1-1': {id: 'unique1-1', content: 'click on "Feature title" for more options'},
+            'unique1-2': {id: 'unique1-2', content: 'click on "PROJECT TITLE" for more options'},
+            'unique1-3': {id: 'unique1-3', content: 'click on "Task" to edit it'},
+            'unique1-4': {id: 'unique1-4', content: 'drag and drop to reorder features and tasks'},
+            'unique1-5': {id: 'unique1-5', content: 'your project will be saved to the local storage of the browser. So it won\'t go away even when page is refreshed or browser is restarted.'},
         },
         //features
         features: {
             'unique1': {
                 id: 'unique1',
-                title: 'test feature',
-                taskIds: ['unique1-1', 'unique1-2'],
+                title: 'Feature title',
+                taskIds: ['unique1-0', 'unique1-1', 'unique1-2'],
                 tasksDone: 0,
                 doneTasks: [],
             },
             'unique2': {
                 id: 'unique2',
-                title: 'test feature2',
-                taskIds: ['unique1-3', 'unique1-4'],
+                title: 'click on "0/2 done" to expand project',
+                taskIds: ['unique1-3', 'unique1-4', 'unique1-5'],
                 tasksDone: 0,
                 doneTasks: [],
             },
